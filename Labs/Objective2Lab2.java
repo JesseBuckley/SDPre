cf5d7e8c8e3ccb93ed1cc17ec17c79d170753f4f
JesseBuckley <jessebuckley2@gmail.com>
2023-10-16 22:11:27 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jesse Buckley";
+    int age = 24;
+    String state = "Ohio";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
