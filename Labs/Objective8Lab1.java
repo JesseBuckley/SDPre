9151eafded5b0ccd5d62ba9766b49cae119be932
JesseBuckley <jessebuckley2@gmail.com>
2023-10-21 11:33:27 -0400

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for(int i=0; i<=10; i+=1){
+      System.out.println(i);
+    }
+    for(int t=10; t>=0; t-=1){ 
+      System.out.println(t);
+    }
+  }
+}
